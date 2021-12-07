db3556858e937ef92c67085f1109373dae7f6a0d
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2021-12-07 13:40:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,43 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 </head>
 <body>
- 
     <table>
         <thead>
             <tr>
-                <th>ID</th><th>First Name</th><th>Last Name</th>
+                <th>ID</th> <th>Title</th> <thAuthorD</th> 
             </tr>
         </thead>
-        <tbody id="tblStudent">
+        <tbody id="tblPost">
+
         </tbody>
     </table>
-</body>
-<script>
- 
-function loadJSON(){
-    var url = "https://cj-android-demon.herokuapp.com/json2.php";
-    $.getJSON(url)
-        .done((data)=>{
-            console.log(data);
-            $.each(data,  (k, item)=>{
-                console.log(k);
-                console.log(item);
-                var line = "<tr>";
-                    line += "<td>" + (k+1) + "</td>";
-                    line += "<td>" + item.fname + "</td>";
-                    line += "<td>" + item.lname + "</td>";
-                    line += "</tr>";
-                $("#tblStudent").append(line);
-            });
+    <script>
+        function loadPost(){
+                var ur ="https://jsonplaceholder.typicode.com/posts";
+                $.getJSON(url)
+                .done((data)=>{
+                    $.each(data, (k , item)=>{
+                        console.log(item);
+                        var line = "<tr";
+                            line += "<td>" + k+"/td";
+                             line += "<td>" + k+"/td";
+                             line += "<td>" + k+"/td";
+                            
+                            line +="</tr>";
+                            $("#tblPosts").append(line);
+
+
+                    });
+                })
+                .fail(xhr, status ,console.error)=>{
+
+                })
+        }
+        $(()=>{
+            loadPost();
         })
-        .fail((xhr, status, err)=>{
-        });
-}
-$(()=>{
-    loadJSON();
-});
-</script>
+    </script>
+
+    
+</body>
 </html>
