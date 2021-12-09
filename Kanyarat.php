4e1485fcdc329aaca65358e16724a1467c3185da
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2021-12-09 13:59:44 +0700

    Create Kanyarat.php

---
+++ Kanyarat.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>63100481</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="col-1"></div>
+    <div class="container">
+        <header>
+            <h1>KPOP</h1>
+            <div class="col-2"></div>
+            <img src="https://media-blog.cdnandroid.com/wp-content/uploads/sites/15/2019/08/12131122/Kpop.jpg" height="250" width="390" class="card-img-top" alt="Responsive image"></h1>
+        </header>
+
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-4">
+                
+                <p><img src="https://www.thewikifeed.com/wp-content/uploads/2021/05/jungkook-1.jpg" class="img-fluid"></p>
+                <h1>จองกุก</h1>
+                <p>จอนจองกุก/ช็อน จ็อง-กุก ( เกาหลี : 전정국 ) เกิดเมื่อวันที่ 1 กันยายน 1997 ที่ปูซานประเทศเกาหลีใต้ ครอบครัวของเขาประกอบด้วยพ่อแม่และพี่ชาย สมัยเด็กจองกุกเคยมีความฝันที่จะเป็นนักแบดมินตัน แต่หลังจากที่ได้เห็นการแสดงของวง G-Dragon ในเพลง " Heartbreaker " ทางโทรทัศน์ก็ทำให้เขาอยากเป็นนักร้อง
+
+                    ในปี 2011 จองกุกได้เข้าร่วมออดิชั่นแสดงความสามารถพิเศษในรายการ Superstar K รอบออดิชั่นที่แดกู แม้ว่าเขาจะไม่ได้รับเลือกให้ผ่านเข้ารอบ แต่นั่นก็ทำให้เขาได้รับข้อเสนอจากบริษัทบันเทิง 7 แห่ง ก่อนที่เขาจะเลือกเป็นเด็กฝึกในสังกัด Big Hit Entertainment เพราะได้เห็นการแสดงของ RM (ซึ่งปัจจุบัน RM ก็ได้กลายมาเป็นหัวหน้าวง BTS) เพื่อฝึกทักษะการเต้นสำหรับเตรียมเดบิวต์ จองกุกได้มีโอกาสเดินทางไปเรียนเต้นที่ลอสแอนเจลิส ในช่วงฤดูร้อนปี 2012 ก่อนจะได้เดบิ้วทอย่างเป็นทางการในฐานะสมาชิกวง BTS ในเดือนมิถุนายน ปี 2013</p>
+            </div>
+            <div class="col-5">
+                <img src="https://media.komchadluek.net/uploads/images/contents/w1024/2021/10/v4ikyoxjdv4Z539nQYr6.jpg" height="250" width="390" class="col"> 
+                <h1>ลิซ่า</h1>
+                <p>ลลิษา มโนบาล[2][3] ชื่อเดิม ปราณปรียา มโนบาล[4] (เกิด 27 มีนาคม พ.ศ. 2540) ชื่อในการแสดงว่า ลิซ่า[5] เป็นแร็ปเปอร์ นักร้อง และนักเต้นชาวไทยซึ่งทำงานหลักในประเทศเกาหลีใต้ เป็นสมาชิกของแบล็กพิงก์ เกิร์ลกรุปเกาหลีใต้สังกัดวายจีเอนเตอร์เทนเมนต์[6]
+
+                    ลิซ่าเปิดตัวเป็นศิลปินเดี่ยวกับซิงเกิลอัลบั้มชื่อ ลลิษา ในเดือนกันยายน พ.ศ. 2564 อัลบั้มเปิดตัวที่อันดับหนึ่งบนแกออนอัลบั้มชาร์ต และขายได้มากกว่า 7 แสนชุดในสัปดาห์แรก ทำให้เธอเป็นศิลปินหญิงเคป็อปคนแรกที่ทำได้[7] มิวสิกวิดีโอเพลงชื่อเดียวกับอัลบั้ม ในยูทูบมีผู้ชมมากกว่า 73.6 ล้านครั้งในวันแรก ทำลายสถิติมิวสิกวิดีโอของศิลปินเดี่ยวที่มีผู้ชมมากที่สุดภายใน 24 ชั่วโมง</p>
+            </div>
+
+        </div>
+    
+        
+    </div>
+    <footer>
+        <b>Kanyarat Rungrueang</b>
+        <b>63100481</b>
+    </footer>
+
+
+
+
+
+
+
+</body>
+</html>
