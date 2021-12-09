9a22f9c2d92bddd6990254d25940336942c2ab27
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2021-12-09 14:02:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,98 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-
+    <title>63100481</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<button id="btnBack"> back </button>
-<div id="main">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th> <th>Title</th><th> Details </th>
-            </tr>
-        </thead>
-        <tbody id="tblPosts">
-        </tbody>
-    </table>
-</div>
+    <div class="col-1"></div>
+    <div class="container">
+        <header>
+            <h1>KPOP</h1>
+            <div class="col-2"></div>
+            <img src="https://media-blog.cdnandroid.com/wp-content/uploads/sites/15/2019/08/12131122/Kpop.jpg" height="250" width="390" class="card-img-top" alt="Responsive image"></h1>
+        </header>
 
-<div id="detail">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th> <th>Title</th><th> userId</th>
-            </tr>
-        </thead>
-        <tbody id="tblDetails">
-        </tbody>
-    </table> 
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-4">
+                
+                <p><img src="https://www.thewikifeed.com/wp-content/uploads/2021/05/jungkook-1.jpg" class="img-fluid"></p>
+                <h1>จองกุก</h1>
+                <p>จอนจองกุก/ช็อน จ็อง-กุก ( เกาหลี : 전정국 ) เกิดเมื่อวันที่ 1 กันยายน 1997 ที่ปูซานประเทศเกาหลีใต้ ครอบครัวของเขาประกอบด้วยพ่อแม่และพี่ชาย สมัยเด็กจองกุกเคยมีความฝันที่จะเป็นนักแบดมินตัน แต่หลังจากที่ได้เห็นการแสดงของวง G-Dragon ในเพลง " Heartbreaker " ทางโทรทัศน์ก็ทำให้เขาอยากเป็นนักร้อง
+
+                    ในปี 2011 จองกุกได้เข้าร่วมออดิชั่นแสดงความสามารถพิเศษในรายการ Superstar K รอบออดิชั่นที่แดกู แม้ว่าเขาจะไม่ได้รับเลือกให้ผ่านเข้ารอบ แต่นั่นก็ทำให้เขาได้รับข้อเสนอจากบริษัทบันเทิง 7 แห่ง ก่อนที่เขาจะเลือกเป็นเด็กฝึกในสังกัด Big Hit Entertainment เพราะได้เห็นการแสดงของ RM (ซึ่งปัจจุบัน RM ก็ได้กลายมาเป็นหัวหน้าวง BTS) เพื่อฝึกทักษะการเต้นสำหรับเตรียมเดบิวต์ จองกุกได้มีโอกาสเดินทางไปเรียนเต้นที่ลอสแอนเจลิส ในช่วงฤดูร้อนปี 2012 ก่อนจะได้เดบิ้วทอย่างเป็นทางการในฐานะสมาชิกวง BTS ในเดือนมิถุนายน ปี 2013</p>
+            </div>
+            <div class="col-5">
+                <img src="https://media.komchadluek.net/uploads/images/contents/w1024/2021/10/v4ikyoxjdv4Z539nQYr6.jpg" height="250" width="390" class="col"> 
+                <h1>ลิซ่า</h1>
+                <p>ลลิษา มโนบาล[2][3] ชื่อเดิม ปราณปรียา มโนบาล[4] (เกิด 27 มีนาคม พ.ศ. 2540) ชื่อในการแสดงว่า ลิซ่า[5] เป็นแร็ปเปอร์ นักร้อง และนักเต้นชาวไทยซึ่งทำงานหลักในประเทศเกาหลีใต้ เป็นสมาชิกของแบล็กพิงก์ เกิร์ลกรุปเกาหลีใต้สังกัดวายจีเอนเตอร์เทนเมนต์[6]
+
+                    ลิซ่าเปิดตัวเป็นศิลปินเดี่ยวกับซิงเกิลอัลบั้มชื่อ ลลิษา ในเดือนกันยายน พ.ศ. 2564 อัลบั้มเปิดตัวที่อันดับหนึ่งบนแกออนอัลบั้มชาร์ต และขายได้มากกว่า 7 แสนชุดในสัปดาห์แรก ทำให้เธอเป็นศิลปินหญิงเคป็อปคนแรกที่ทำได้[7] มิวสิกวิดีโอเพลงชื่อเดียวกับอัลบั้ม ในยูทูบมีผู้ชมมากกว่า 73.6 ล้านครั้งในวันแรก ทำลายสถิติมิวสิกวิดีโอของศิลปินเดี่ยวที่มีผู้ชมมากที่สุดภายใน 24 ชั่วโมง</p>
+            </div>
+
+        </div>
     
-</div>
-</body>
-
-<script>
-    function showDetails(id){
-        $("#main").hide();
-        $("#detail").show();
-        var url = "https://jsonplaceholder.typicode.com/posts/"+id;
-        $.getJSON(url)
-            .done((data)=>{
-                console.log(data);
-                var line = "<tr id='rowdetail'";
-                        line += "<td>"+ data.id + "</td>";
-                        line += "<td><b>"+ data.title + "</b><br/>";
-                        line += data.body + "</td>";
-                        line += "<td>" + data.userId + "</td>"
-
-                        line += "</tr>";
-                    $("#tblDetails").append(line);
-
-            })
-            .fail((xhr, status, error)=>{
-
-            })
-    }
-    function loadPosts(){
-        $("#main").show();
-        $("#details").hide();
         
-        var url = "https://jsonplaceholder.typicode.com/posts";
-        $.getJSON(url)
-            .done((data)=>{
-                $.each(data, (k, item)=>{
-                    console.log(item);
-                    var line = "<tr>";
-                        line += "<td>"+ item.id + "</td>";
-                        line += "<td><b>"+ item.title + "</b><br/>";
-                        line += item.body + "</td>";
-                        line += "<td> <button onClick='showDetails("+ item.id +");' > link </button> </td>";
+    </div>
+    <footer>
+        <b>Kanyarat Rungrueang</b>
+        <b>63100481</b>
+    </footer>
 
-                        line += "</tr>";
-                    $("#tblPosts").append(line);
-                });
-                $("#main").show();
-            })
-            .fail((xhr, status, error)=>{
 
-            })
-    }
-  
-    $(()=>{
 
-        loadPosts();
-        $("#btnBack").click(()=>{
-            $("#main").show();
-            $("#detail").hide();
-            $("#rowdetail").remove();
-            
-       
-        });
-    
-    })
 
-</script>
+
+
+
+</body>
 </html>
